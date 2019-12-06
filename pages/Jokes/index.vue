e4954cd96d40c6294bd5a1fac7de3fc929106fcf
Halina <hbalys@gmail.com>
2019-12-06 14:46:04 +0100

    get jokes from API by axios

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -3,11 +3,40 @@
 </template>
 
 <script>
-export default {
+import axios from "axios";
 
-}
+export default {
+  data() {
+    return {
+      jokes: []
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    };
+    try {
+      const res = await axios.get("http://api.icndb.com/jokes/random/10", config);
+      this.jokes = res.data.value;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  head() {
+    return {
+      title: "Chuck Norris jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for Chuck Norris jokes"
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
