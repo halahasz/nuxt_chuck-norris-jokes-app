e6e1533ae942f830873ba618f6cef0f176795f89
Halina <hbalys@gmail.com>
2019-12-06 14:00:35 +0100

    initial commit

---
+++ pages/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About Chuck Norris jokes</h1>
+    <p>This is an app that displays Chuck Norris jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About The App",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for Chuck Norris jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
